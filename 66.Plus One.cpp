6824740323b09c4e17f7cb7d8daddd7d9bfbfcaa
Atharva-211 <atharva.gaikwad211@gmail.com>
2025-12-03 03:00:28 +0530

    test

--- 66.Plus One.cpp
+++ 66.Plus One.cpp
@@ -1,40 +1,27 @@
-#include <iostream>
-#include <stdio.h>
-#include <conio.h>
-#include <string>
-#include <vector>
-#include <map>
-#include <unordered_set>
-#include <stack>
-#include <queue>
-#include <array>
-#include <algorithm>
-
-using namespace std;
-
-
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         vector <int> result;
         int carry =1;
 
-        for(int i = digits.size() -1; i <= 0 ; i--){
+        for (int i = digits.size() - 1; i >= 0; i--){
             if(digits[i] != 9 && carry == 1){
-                result.push_back(digits[i] + 1);
-                carry = 0;
+                // result.push_back(digits[i] + 1);
+                result.insert(result.begin(),(digits[i] + 1));
+
+                carry = 0; i--;
+                while(i>=0){
+                    result.insert(result.begin(),digits[i--]);
+                } return result;
             } else if(digits[i] == 9){
-                result.push_back(digits[i] + 1);
+                // result.push_back(digits[i] + 1);
+                result.insert(result.begin(),0);
                 carry = 1;
             }
+            
         }
-
+            result.insert(result.begin(),1);
         return result;
         
     }
 };
-
-int main()
-{
-    return 0;
-}
